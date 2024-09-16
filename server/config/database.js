805ca0eb99363a6fb625b1909f4c5a448b6d7ce6
“Dennis <dennisnzioki019@gmail.com>
2024-09-16 18:57:20 +0300

    Create database.js

---
+++ server/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+exports.connect = () => {
+	mongoose
+		.connect(`${process.env.MONGODB_URL}`, {
+			useNewUrlparser: true,
+			useUnifiedTopology: true,
+		})
+		.then(console.log(`DB Connection Success`))
+		.catch((err) => {
+			console.log(`DB Connection Failed`);
+			console.log(err);
+			process.exit(1);
+		});
+};
