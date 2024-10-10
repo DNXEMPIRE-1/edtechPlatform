d835210294d420fd58a4ca1b329d6b2b816e828a
DNXEMPIRE-1 <89406534+DNXEMPIRE-1@users.noreply.github.com>
2024-10-11 00:02:52 +0300

    Update LearningGrid.jsx

--- src/components/core/AboutPage/LearningGrid.jsx
+++ src/components/core/AboutPage/LearningGrid.jsx
@@ -8,7 +8,7 @@ const LearningGridArray = [
     heading: "World-Class Learning for",
     highliteText: "Anyone, Anywhere",
     description:
-      "Maverix partners with more than 275+ leading universities and companies to bring flexible, affordable, job-relevant online learning to individuals and organizations worldwide.",
+      "BrightPath partners with more than 275+ leading universities and companies to bring flexible, affordable, job-relevant online learning to individuals and organizations worldwide.",
     BtnText: "Learn More",
     BtnLink: "/",
   },
@@ -22,25 +22,25 @@ const LearningGridArray = [
     order: 2,
     heading: "Our Learning Methods",
     description:
-      "Maverix partners with more than 275+ leading universities and companies to bring",
+      "BrightPath partners with more than 275+ leading universities and companies to bring",
   },
   {
     order: 3,
     heading: "Certification",
     description:
-      "Maverix partners with more than 275+ leading universities and companies to bring",
+      "BrightPath partners with more than 275+ leading universities and companies to bring",
   },
   {
     order: 4,
     heading: `Rating "Auto-grading"`,
     description:
-      "Maverix partners with more than 275+ leading universities and companies to bring",
+      "BrightPath partners with more than 275+ leading universities and companies to bring",
   },
   {
     order: 5,
     heading: "Ready to Work",
     description:
-      "Maverix partners with more than 275+ leading universities and companies to bring",
+      "BrightPath partners with more than 275+ leading universities and companies to bring",
   },
 ];
 
