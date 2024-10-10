28e194049320f7434822d5be189bb35436294255
DNXEMPIRE-1 <89406534+DNXEMPIRE-1@users.noreply.github.com>
2024-10-11 00:03:21 +0300

    Update emailVerificationTemplate.js

--- server/mail/templates/emailVerificationTemplate.js
+++ server/mail/templates/emailVerificationTemplate.js
@@ -1,7 +1,7 @@
 const otpTemplate = (otp) => {
 	return `<!DOCTYPE html>
 	<html>
-	
+
 	<head>
 		<meta charset="UTF-8">
 		<title>OTP Verification Email</title>
@@ -15,30 +15,30 @@ const otpTemplate = (otp) => {
 				margin: 0;
 				padding: 0;
 			}
-	
+
 			.container {
 				max-width: 600px;
 				margin: 0 auto;
 				padding: 20px;
 				text-align: center;
 			}
-	
+
 			.logo {
 				max-width: 200px;
 				margin-bottom: 20px;
 			}
-	
+
 			.message {
 				font-size: 18px;
 				font-weight: bold;
 				margin-bottom: 20px;
 			}
-	
+
 			.body {
 				font-size: 16px;
 				margin-bottom: 20px;
 			}
-	
+
 			.cta {
 				display: inline-block;
 				padding: 10px 20px;
@@ -50,37 +50,37 @@ const otpTemplate = (otp) => {
 				font-weight: bold;
 				margin-top: 20px;
 			}
-	
+
 			.support {
 				font-size: 14px;
 				color: #999999;
 				margin-top: 20px;
 			}
-	
+
 			.highlight {
 				font-weight: bold;
 			}
 		</style>
-	
+
 	</head>
-	
+
 	<body>
 		<div class="container">
-			
+
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
 				<p>Dear User,</p>
-				<p>Thank you for registering with Maverix. To complete your registration, please use the following OTP
+				<p>Thank you for registering with BrightPath. To complete your registration, please use the following OTP
 					(One-Time Password) to verify your account:</p>
 				<h2 class="highlight">${otp}</h2>
 				<p>This OTP is valid for 5 minutes. If you did not request this verification, please disregard this email.
 				Once your account is verified, you will have access to our platform and its features.</p>
 			</div>
 			<div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-					href="mailto:info@maverix.com">info@maverix.com</a>. We are here to help!</div>
+					href="mailto:info@dennisnzioki.com">info@dennisnzioki.com</a>. We are here to help!</div>
 		</div>
 	</body>
-	
+
 	</html>`;
 };
 module.exports = otpTemplate;
