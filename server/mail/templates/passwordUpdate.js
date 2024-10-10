5b0c1c4a298eef038e340809a28a0ea9a03f87a9
DNXEMPIRE-1 <89406534+DNXEMPIRE-1@users.noreply.github.com>
2024-10-11 00:03:25 +0300

    Update passwordUpdate.js

--- server/mail/templates/passwordUpdate.js
+++ server/mail/templates/passwordUpdate.js
@@ -1,7 +1,7 @@
 exports.passwordUpdated = (email, name) => {
 	return `<!DOCTYPE html>
     <html>
-    
+
     <head>
         <meta charset="UTF-8">
         <title>Password Update Confirmation</title>
@@ -15,47 +15,47 @@ exports.passwordUpdated = (email, name) => {
                 margin: 0;
                 padding: 0;
             }
-    
-    
+
+
             .container {
                 max-width: 600px;
                 margin: 0 auto;
                 padding: 20px;
                 text-align: center;
             }
-    
+
             .logo {
                 max-width: 200px;
                 margin-bottom: 20px;
             }
-    
+
             .message {
                 font-size: 18px;
                 font-weight: bold;
                 margin-bottom: 20px;
             }
-    
+
             .body {
                 font-size: 16px;
                 margin-bottom: 20px;
             }
-    
+
             .support {
                 font-size: 14px;
                 color: #999999;
                 margin-top: 20px;
             }
-    
+
             .highlight {
                 font-weight: bold;
             }
         </style>
-    
+
     </head>
-    
+
     <body>
         <div class="container">
-            
+
             <div class="message">Password Update Confirmation</div>
             <div class="body">
                 <p>Hey ${name},</p>
@@ -65,10 +65,10 @@ exports.passwordUpdated = (email, name) => {
             </div>
             <div class="support">If you have any questions or need further assistance, please feel free to reach out to us
                 at
-                <a href="mailto:info@maverix.com">info@maverix.com</a>. We are here to help!
+                <a href="mailto:info@dennisnzioki.com">info@dennisnzioki.com</a>. We are here to help!
             </div>
         </div>
     </body>
-    
+
     </html>`;
 };
