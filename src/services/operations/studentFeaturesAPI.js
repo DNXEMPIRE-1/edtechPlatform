a5e15b03acc37e177705d299694bdce7d31a73d0
DNXEMPIRE-1 <89406534+DNXEMPIRE-1@users.noreply.github.com>
2024-10-11 00:02:45 +0300

    Update studentFeaturesAPI.js

--- src/services/operations/studentFeaturesAPI.js
+++ src/services/operations/studentFeaturesAPI.js
@@ -70,7 +70,7 @@ export async function BuyCourse(
       currency: orderResponse.data.data.currency,
       amount: `${orderResponse.data.data.amount}`,
       order_id: orderResponse.data.data.id,
-      name: "Maverix",
+      name: "BrightPath",
       description: "Thank you for Purchasing the Course.",
       image: rzpLogo,
       prefill: {
